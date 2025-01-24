71fd31205c20fe948238a2ae7ac2f74c08189599
SudeshRPatil20 <Sudeshrpatil201@gmail.com>
2025-01-24 18:57:38 +0530

    Model Traininer

--- src/components/data_ingesion.py
+++ src/components/data_ingesion.py
@@ -10,6 +10,11 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_training import ModelTrainerConfig
+from src.components.model_training import ModelTrainer
+
+
+
 @dataclass
 class DataIngesionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -52,4 +57,8 @@ if __name__=="__main__":
     train_data, test_data=obj.initiate_data_ingesion()
     
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
