6a8067430b544480a7ba1038e6dd94e8d610d394
Sudesh Patil <SUDESHRPATIL201@GMAIL.COM>
2025-10-27 01:05:36 +0530

    Update application.py

--- application.py
+++ application.py
@@ -50,5 +50,6 @@ def predict_datapoint():
 #     from vercel_py_adapter import VercelAdapter
 #     return VercelAdapter(app).handle(event, context)
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # use Render's PORT or default 5000
+    app.run(host="0.0.0.0", port=port)
